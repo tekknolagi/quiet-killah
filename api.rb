d971cab1be48d3352fa432263697a7dbe532bc01
Max Bernstein <tekknolagi@gmail.com>
2014-10-25 08:35:30 -0400

    fix responses yet again

--- api.rb
+++ api.rb
@@ -6,15 +6,17 @@ class ClassRanker
     format :json
 
     get '/worst' do
-      { :class => $classes.max_by do |klass|
-          klass[1]
-        end[0] }
+      $classes.max_by do |klass|
+        klass[1]
+      end
+      { :class => klass[0], :rank => klass[1] }
     end
 
     get '/best' do
-      { :class => $classes.min_by do |klass|
-          klass[1]
-        end[0] }
+      klass = $classes.min_by do |klass|
+        klass[1]
+      end
+      { :class => klass[0], :rank => klass[1] }
     end
 
     params do

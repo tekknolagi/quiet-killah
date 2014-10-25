d5b8b8dd478ccc169071d1d8db883d684a4d9e79
Max Bernstein <tekknolagi@gmail.com>
2014-10-25 08:31:30 -0400

    fix the switch from ages ago

--- api.rb
+++ api.rb
@@ -6,13 +6,13 @@ class ClassRanker
     format :json
 
     get '/worst' do
-      { :class => $classes.min_by do |klass|
+      { :class => $classes.max_by do |klass|
         klass[1]
         end }
     end
 
     get '/best' do
-      { :class => $classes.max_by do |klass|
+      { :class => $classes.min_by do |klass|
         klass[1]
         end }
     end
